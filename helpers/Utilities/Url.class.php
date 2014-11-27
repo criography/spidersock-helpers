58a8e8c1c9027a75a9871da53d60bba3f7a772d5
Marek Lenik <marek@aardvarklondon.com>
2014-11-27 10:39:08 +0000

    + Generic URL helpers, parse_url for relative URLs fix

---
+++ helpers/Utilities/Url.class.php
@@ -0,0 +1,71 @@
+<?php
+
+    /**
+     * SSK_Utils_File
+     * ---------------------------------------------------------------------
+     * URL Utilities
+     *
+     * @package      SpiderSock Helpers
+     * @subpackage   Utilities
+     * @since        2.0
+     */
+    class SSK_Utils_File
+    {
+
+
+        /**
+         * init
+         * ---------------------------------------------------------------------
+         * initiate class and expose its methods
+         *
+         * @return void
+         */
+        private static function init()
+        {
+            $class = __CLASS__;
+            new $class;
+        }
+
+
+
+
+
+
+        /**
+         * __construct
+         * ---------------------------------------------------------------------
+         */
+        public function __construct()
+        {
+        }
+
+
+
+
+
+
+        /**
+         * parse_url
+         *
+         * Temporary fix for protocol relative URLs, not supported by native PHP function
+         *
+         * @param     string $url URL
+         *
+         * @return    array                   should be identical as he output of parse_url
+         */
+        public static function parse_url( $url )
+        {
+            if (!empty( $url ) && is_string($url) && strpos($url, '//') === 0) {
+                $url = ( (!empty($_SERVER[ 'HTTPS' ]) && $_SERVER['HTTPS']!=='off' || $_SERVER['SERVER_PORT']==443) ? "https:" : "http:" ) . $url;
+            }
+
+            $_url = parse_url($url);
+
+            return $_url;
+        }
+
+
+    }
+
+    new SSK_Utils_Url;
+    //add_action('plugins_loaded', array('SSK_Utils_Url', 'init'));
--- spidersock-helpers.php
+++ spidersock-helpers.php
@@ -4,7 +4,7 @@ Plugin Name: SpiderSock Helpers
 Plugin URI: http:/criography.com/
 Description: Pile fo various (WP and not only) helpers
 Author: Marek Lenik
-Version: 1.0
+Version: 1.01
 Author URI: http:/criography.com/
 
 Copyright 2014 Marek Lenik
@@ -15,6 +15,7 @@ define('SOCKHELPERS', __DIR__. '/helpers/');
 require_once('helpers/Image/Blending.class.php');
 require_once('helpers/Image/Image.class.php');
 require_once('helpers/Text/Text.class.php');
+require_once('helpers/Utilities/Url.class.php');
 require_once('helpers/Utilities/File.class.php');
 require_once('helpers/Utilities/Utils.class.php');
 require_once('helpers/Wordpress/Utils/Url.class.php');
